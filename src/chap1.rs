2c6c089918f9a343172293dd6d8eb5ef842aefc0
Dave King <tildedave@gmail.com>
2025-01-04 11:45:05 -0500

    sqrt_mod_p implemented (and works!)

--- src/chap1.rs
+++ src/chap1.rs
@@ -1,13 +1,19 @@
 extern crate num;
+extern crate rand;
 
 use self::num::One;
 use self::num::Signed;
 use self::num::Zero;
+use self::rand::distributions::{Distribution, Standard};
+use self::rand::thread_rng;
+use self::rand::Rng;
 use std::cmp::PartialOrd;
+use std::fmt::Display;
 use std::ops::BitAnd;
 use std::ops::Div;
 use std::ops::Mul;
 use std::ops::Rem;
+use std::ops::Shl;
 use std::ops::Shr;
 use std::ops::Sub;
 
@@ -25,6 +31,18 @@ where
         }
     }
     return n;
+}
+
+fn mod_positive<T>(n: T, a: T) -> T
+where
+    T: Rem<Output = T> + Sub<Output = T> + PartialOrd + Signed + Copy,
+{
+    let m = n % a;
+    if m < T::zero() {
+        m + a
+    } else {
+        m
+    }
 }
 
 // Return the individual prime factors associated with a number
@@ -48,13 +66,25 @@ fn prime_factor(n: i32) -> Vec<i32> {
 }
 
 // Algorithm 1.2.1 (Right-Left Binary) pg 8
-fn mod_exp(g: i32, n: i32, p: i32) -> i32 {
-    let mut y = 1;
+fn mod_exp<T>(g: T, n: T, p: T) -> T
+where
+    T: Zero
+        + One
+        + Sub<Output = T>
+        + Copy
+        + PartialEq
+        + PartialOrd
+        + std::ops::Neg<Output = T>
+        + Rem<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>,
+{
+    let mut y = T::one();
     let mut z;
     let mut exp;
-    if n == 0 {
+    if n == T::zero() {
         return y;
-    } else if n < 0 {
+    } else if n < T::zero() {
         exp = -n;
         z = mod_inverse(g, p);
     } else {
@@ -62,12 +92,12 @@ fn mod_exp(g: i32, n: i32, p: i32) -> i32 {
         z = g;
     }
 
-    while exp > 0 {
-        if exp % 2 == 1 {
+    while exp > T::zero() {
+        if is_odd(exp) {
             y = (y * z) % p;
         }
         z = (z * z) % p;
-        exp = exp / 2;
+        exp = exp / (T::one() + T::one());
     }
 
     return y;
@@ -102,8 +132,20 @@ fn mod_exp2(g: i32, n: i32, p: i32) -> i32 {
     return y;
 }
 
-fn mod_inverse(m: i32, p: i32) -> i32 {
-    return mod_exp(m, p - 2, p);
+fn mod_inverse<T>(m: T, p: T) -> T
+where
+    T: Zero
+        + One
+        + Sub<Output = T>
+        + Copy
+        + PartialEq
+        + PartialOrd
+        + std::ops::Neg<Output = T>
+        + Rem<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>,
+{
+    return mod_exp(m, p - T::one() - T::one(), p);
 }
 
 // Algorithm 1.3.1 (Euclid GCD)
@@ -372,10 +414,13 @@ where
 }
 
 // 1.7. Integer Square Root
-fn isqrt(n: i32) -> i32 {
+fn isqrt<T>(n: T) -> T
+where
+    T: Zero + One + Div<Output = T> + PartialOrd + Copy,
+{
     let mut x = n;
     loop {
-        let y = (x + (n / x)) >> 1;
+        let y = (x + (n / x)) / (T::one() + T::one());
         if y >= x {
             return x;
         }
@@ -385,9 +430,9 @@ fn isqrt(n: i32) -> i32 {
 }
 
 // Algorithm 1.4.10 (Kronecker symbol) pg 29
-fn kroneker_symbol<T>(a: T, b: T) -> i32
+pub fn kronecker_symbol<T>(a: T, b: T) -> i32
 where
-    T: Zero + One + Signed + BitAnd<Output = T> + PartialOrd + Copy,
+    T: Zero + One + Signed + BitAnd<Output = T> + PartialOrd + Copy + Display,
 {
     let (zero, one, two, three) = (
         T::zero(),
@@ -398,7 +443,7 @@ where
     let (four, five) = (two + two, three + two);
     let (six, seven) = (three + three, three + four);
 
-    let kroneker_table = |x| {
+    let kronecker_table = |x| {
         if x == zero {
             0
         } else if x == one {
@@ -438,10 +483,10 @@ where
     }
 
     // a&7 = (-1)^((a^2 - 1)/8)
-    let mut k = if v % two == T::zero() {
+    let mut k = if is_even(v) {
         1
     } else {
-        kroneker_table(a & seven)
+        kronecker_table(a & seven)
     };
 
     if b < T::zero() {
@@ -452,11 +497,17 @@ where
     }
 
     loop {
-        assert!(b % two != T::zero());
+        assert!(is_odd(b));
         assert!(b > T::zero());
 
         if a == T::zero() {
-            return if b > T::one() { 0 } else { k };
+            return if b > T::one() {
+                0
+            } else if b == T::one() {
+                k
+            } else {
+                panic!("Impossible")
+            };
         }
 
         v = T::zero();
@@ -464,8 +515,8 @@ where
             v = v + one;
             a = a / two;
         }
-        if !is_even(v) {
-            k = kroneker_table(b & seven);
+        if is_odd(v) {
+            k = kronecker_table(b & seven) * k;
         }
 
         // reciprocity
@@ -477,6 +528,146 @@ where
         let r = a.abs();
         a = b % r;
         b = r;
+    }
+}
+
+// Tonelli/Shanks Algorithm for modular square root
+// Adapted from an implementation I did in Python
+// Algo 1.5.1
+pub fn sqrt_mod_p<T>(a: T, p: T) -> Result<T, String>
+where
+    T: Zero
+        + One
+        + Sub<Output = T>
+        + Rem<Output = T>
+        + Div<Output = T>
+        + PartialEq
+        + Copy
+        + Signed
+        + PartialOrd
+        + BitAnd<Output = T>
+        + Shl<Output = T>
+        + Display, // remove me
+    Standard: Distribution<T>,
+{
+    let mut rng = thread_rng();
+
+    let two = T::one() + T::one();
+    let mut e = T::zero();
+    let mut q = p - T::one();
+
+    while is_even(q) {
+        e = e + T::one();
+        q = q / two;
+    }
+    assert!(is_odd(q), "Reduced value should have been odd");
+
+    // Step 1 - find generator
+    let n: T = loop {
+        let n: T = mod_positive(rng.gen(), p);
+        if n == T::one() {
+            continue;
+        }
+        if kronecker_symbol(n, p) == -1 {
+            break n;
+        }
+    };
+
+    // Step 2 - initialize
+    let mut z = mod_exp(n, q, p);
+    let mut y = z;
+    let mut r = e;
+
+    let x = mod_exp(a, (q - T::one()) / two, p);
+    let mut b = mod_positive(a * x * x, p);
+    let mut x = mod_positive(a * x, p);
+
+    loop {
+        // Step 3 - find exponent
+        if b % p == T::one() {
+            break Ok(x);
+        }
+
+        // find smallest m such that b^2^m = 1 (p)
+        // if m = r output that a is a non-residue
+        let mut m = T::one();
+        while mod_exp(b, T::one() << m, p) != T::one() {
+            m = m + T::one();
+        }
+
+        if m == r {
+            break Err(format!("Not a quadratic residue mod {}", p));
+        }
+
+        // Step 4 - reduce exponent
+        let t: T = mod_exp(y, T::one() << (r - m - T::one()), p);
+        y = mod_positive((t * t), p);
+        r = m;
+        x = mod_positive(x * t, p);
+        b = mod_positive(b * y, p);
+    }
+}
+
+// 1.5.2
+// Let p be a prime number and d an integer so that 0 < d < p.  This algorithm
+// outputs either an integer solution (x, y) so that x^2 + dy^2 = p, or says
+// that such a solution does not exist.
+pub fn cornacchia_algorithm<T>(d: T, p: T) -> Result<(T, T), String>
+where
+    T: Zero
+        + One
+        + Sub<Output = T>
+        + Rem<Output = T>
+        + Div<Output = T>
+        + PartialEq
+        + Copy
+        + Signed
+        + PartialOrd
+        + BitAnd<Output = T>
+        + Shl<Output = T>
+        + Display,
+    Standard: Distribution<T>,
+{
+    let k = kronecker_symbol(-d, p);
+    if k == -1 {
+        Err("Equation has no solution".to_string())
+    } else {
+        let mut a = p;
+        let mut b = match sqrt_mod_p(-d, p) {
+            Err(_) => panic!("did not find square root despite ({}, {}) = {}", -d, p, k),
+            Ok(mut x0) => {
+                // so now x^0 \equiv -d mod p
+                if x0 < T::zero() {
+                    x0 = -x0;
+                }
+                if k == 1 {
+                    x0 = x0 + p;
+                }
+                x0
+            }
+        };
+        let l = isqrt(p);
+        b = loop {
+            if b <= l {
+                break b;
+            } else {
+                let r = a % b;
+                a = b;
+                b = r;
+            }
+        };
+        let num = p - b * b;
+        if num % d != T::zero() {
+            Err("Equation has no solution".to_string())
+        } else {
+            let c = (p - b * b) / d;
+            let sqrt_c = isqrt(c);
+            if sqrt_c * sqrt_c != c {
+                Err("Equation has no solution".to_string())
+            } else {
+                Ok((b, sqrt_c))
+            }
+        }
     }
 }
 
@@ -566,16 +757,36 @@ mod tests {
 
     #[test]
     fn test_kronecker_symbol() {
-        assert_eq!(-1, kroneker_symbol(-1, 7));
-        assert_eq!(1, kroneker_symbol(-1, 13));
-        assert_eq!(-1, kroneker_symbol(2, 3));
-        assert_eq!(-1, kroneker_symbol(2, 5));
-        assert_eq!(-1, kroneker_symbol(2, 11));
-        assert_eq!(-1, kroneker_symbol(2, 13));
-        assert_eq!(1, kroneker_symbol(2, 7));
-        assert_eq!(1, kroneker_symbol(2, 17));
-        assert_eq!(1, kroneker_symbol(2, 23));
-        assert_eq!(-1, kroneker_symbol(23, 59));
+        assert_eq!(-1, kronecker_symbol(-1, 7));
+        assert_eq!(1, kronecker_symbol(-1, 13));
+        assert_eq!(-1, kronecker_symbol(2, 3));
+        assert_eq!(-1, kronecker_symbol(2, 5));
+        assert_eq!(-1, kronecker_symbol(2, 11));
+        assert_eq!(-1, kronecker_symbol(2, 13));
+        assert_eq!(1, kronecker_symbol(2, 7));
+        assert_eq!(1, kronecker_symbol(2, 17));
+        assert_eq!(1, kronecker_symbol(2, 23));
+        assert_eq!(-1, kronecker_symbol(23, 59));
+        assert_eq!(1, kronecker_symbol(43, 97));
+        // assert_eq!(1, kronecker_symbol(0, 97));
+    }
+
+    #[test]
+    fn test_quadratic_residues() {
+        // https://doc.sagemath.org/html/en/constructions/number_theory.html#quadratic-residues
+        // Not clear why the kronecker symbol algo is returning 0 for (0 / 23).
+        assert_eq!(
+            vec![1, 2, 3, 4, 6, 8, 9, 12, 13, 16, 18],
+            (1..23)
+                .filter(|x| kronecker_symbol(*x, 23) == 1)
+                .collect::<Vec<i32>>()
+        );
+        assert_eq!(
+            vec![5, 7, 10, 11, 14, 15, 17, 19, 20, 21],
+            (1..22)
+                .filter(|x| kronecker_symbol(*x, 23) == -1)
+                .collect::<Vec<i32>>()
+        );
     }
 
     #[test]
@@ -607,5 +818,26 @@ mod tests {
                 }
             )
         );
+    }
+
+    #[test]
+    fn test_sqrt_mod_p() {
+        let res = sqrt_mod_p(4, 7);
+        assert!(res == Ok(2) || res == Ok(5));
+        let res = sqrt_mod_p(2, 7);
+        assert!(res == Ok(3) || res == Ok(4));
+        let res: Result<i32, String> = sqrt_mod_p(58, 101);
+        assert!(res == Ok(82) || res == Ok(19));
+        let res: Result<i32, String> = sqrt_mod_p(-2, 97);
+        assert!(res == Ok(17) || res == Ok(80));
+
+        let res = sqrt_mod_p(5, 7);
+        assert_eq!(res, Err("Not a quadratic residue mod 7".to_string()));
+    }
+
+    #[test]
+    fn test_cornacchia() {
+        println!("beep");
+        assert_eq!(cornacchia_algorithm(2, 97), Ok((5, 6)));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod chap1;
 
 fn main() {
     println!("Hello world!");
+    println!("{}", chap1::kronecker_symbol(23, 59));
 }
